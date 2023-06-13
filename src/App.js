6dfc493a69a74d481099473e07d0c945eee95588
TAMS-TECHNOLOGY <dummytwiz@gmail.com>
2023-06-13 13:20:27 +0100

    add other features

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Calculator from './Components/Calculator';
 import Displayqoutes from './Components/DisplayQuotes/Displayqoutes';
+import Navbar from './Components/Navbar/Navbar';
+import Home from './Components/Home/Home';
 
 function App() {
   return (
-    <div className="App">
-      <Calculator />
-      <Displayqoutes />
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/calculator" element={<Calculator />} />
+        <Route exact path="/qoutes" element={<Displayqoutes />} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/Components/Calculator.jsx
+++ src/Components/Calculator.jsx
@@ -18,176 +18,181 @@ function Calculator() {
   const { total, next, operation } = state;
   return (
     <div className="calculator_container">
-      <div className="calculator-body">
-        <div className="display-screen">
-          <span>
-            {total}
-            {operation}
-            {next}
-          </span>
-        </div>
-        <div className="calculator-buttons">
-          <div className="row">
-            <button
-              name="AC"
-              type="button"
-              className="btns"
-              onClick={clickHandle}
-            >
-              AC
-            </button>
-            <button
-              name="+/-"
-              type="button"
-              className="btns"
-              onClick={clickHandle}
-            >
-              +/-
-            </button>
-            <button
-              name="%"
-              type="button"
-              className="btns"
-              onClick={clickHandle}
-            >
-              %
-            </button>
-            <button
-              name="÷"
-              type="button"
-              className="btns coloured-btn"
-              onClick={clickHandle}
-            >
-              &divide;
-            </button>
+      <div className="page-text">
+        <h3 className="text">Let do Some Maths</h3>
+      </div>
+      <div className="calculator_part">
+        <div className="calculator-body">
+          <div className="display-screen">
+            <span>
+              {total}
+              {operation}
+              {next}
+            </span>
           </div>
-          <div className="row">
-            <button
-              name="7"
-              type="button"
-              className="btns"
-              onClick={clickHandle}
-            >
-              7
-            </button>
-            <button
-              name="8"
-              type="button"
-              className="btns"
-              onClick={clickHandle}
-            >
-              8
-            </button>
-            <button
-              name="9"
-              type="button"
-              className="btns"
-              onClick={clickHandle}
-            >
-              9
-            </button>
-            <button
-              name="x"
-              type="button"
-              className="btns coloured-btn"
-              onClick={clickHandle}
-            >
-              x
-            </button>
-          </div>
-          <div className="row">
-            <button
-              name="4"
-              type="button"
-              className="btns"
-              onClick={clickHandle}
-            >
-              4
-            </button>
-            <button
-              name="5"
-              type="button"
-              className="btns"
-              onClick={clickHandle}
-            >
-              5
-            </button>
-            <button
-              name="6"
-              type="button"
-              className="btns"
-              onClick={clickHandle}
-            >
-              6
-            </button>
-            <button
-              name="-"
-              type="button"
-              className="btns coloured-btn"
-              onClick={clickHandle}
-            >
-              -
-            </button>
-          </div>
-          <div className="row">
-            <button
-              name="1"
-              type="button"
-              className="btns"
-              onClick={clickHandle}
-            >
-              1
-            </button>
-            <button
-              name="2"
-              type="button"
-              className="btns"
-              onClick={clickHandle}
-            >
-              2
-            </button>
-            <button
-              name="3"
-              type="button"
-              className="btns"
-              onClick={clickHandle}
-            >
-              3
-            </button>
-            <button
-              name="+"
-              type="button"
-              className="btns coloured-btn"
-              onClick={clickHandle}
-            >
-              +
-            </button>
-          </div>
-          <div className="row">
-            <button
-              name="0"
-              type="button"
-              className="btns zero-btn"
-              onClick={clickHandle}
-            >
-              0
-            </button>
-            <button
-              name="."
-              type="button"
-              className="btns"
-              onClick={clickHandle}
-            >
-              .
-            </button>
-            <button
-              name="="
-              type="button"
-              className="btns coloured-btn"
-              onClick={clickHandle}
-            >
-              =
-            </button>
+          <div className="calculator-buttons">
+            <div className="row">
+              <button
+                name="AC"
+                type="button"
+                className="btns"
+                onClick={clickHandle}
+              >
+                AC
+              </button>
+              <button
+                name="+/-"
+                type="button"
+                className="btns"
+                onClick={clickHandle}
+              >
+                +/-
+              </button>
+              <button
+                name="%"
+                type="button"
+                className="btns"
+                onClick={clickHandle}
+              >
+                %
+              </button>
+              <button
+                name="÷"
+                type="button"
+                className="btns coloured-btn"
+                onClick={clickHandle}
+              >
+                &divide;
+              </button>
+            </div>
+            <div className="row">
+              <button
+                name="7"
+                type="button"
+                className="btns"
+                onClick={clickHandle}
+              >
+                7
+              </button>
+              <button
+                name="8"
+                type="button"
+                className="btns"
+                onClick={clickHandle}
+              >
+                8
+              </button>
+              <button
+                name="9"
+                type="button"
+                className="btns"
+                onClick={clickHandle}
+              >
+                9
+              </button>
+              <button
+                name="x"
+                type="button"
+                className="btns coloured-btn"
+                onClick={clickHandle}
+              >
+                x
+              </button>
+            </div>
+            <div className="row">
+              <button
+                name="4"
+                type="button"
+                className="btns"
+                onClick={clickHandle}
+              >
+                4
+              </button>
+              <button
+                name="5"
+                type="button"
+                className="btns"
+                onClick={clickHandle}
+              >
+                5
+              </button>
+              <button
+                name="6"
+                type="button"
+                className="btns"
+                onClick={clickHandle}
+              >
+                6
+              </button>
+              <button
+                name="-"
+                type="button"
+                className="btns coloured-btn"
+                onClick={clickHandle}
+              >
+                -
+              </button>
+            </div>
+            <div className="row">
+              <button
+                name="1"
+                type="button"
+                className="btns"
+                onClick={clickHandle}
+              >
+                1
+              </button>
+              <button
+                name="2"
+                type="button"
+                className="btns"
+                onClick={clickHandle}
+              >
+                2
+              </button>
+              <button
+                name="3"
+                type="button"
+                className="btns"
+                onClick={clickHandle}
+              >
+                3
+              </button>
+              <button
+                name="+"
+                type="button"
+                className="btns coloured-btn"
+                onClick={clickHandle}
+              >
+                +
+              </button>
+            </div>
+            <div className="row">
+              <button
+                name="0"
+                type="button"
+                className="btns zero-btn"
+                onClick={clickHandle}
+              >
+                0
+              </button>
+              <button
+                name="."
+                type="button"
+                className="btns"
+                onClick={clickHandle}
+              >
+                .
+              </button>
+              <button
+                name="="
+                type="button"
+                className="btns coloured-btn"
+                onClick={clickHandle}
+              >
+                =
+              </button>
+            </div>
           </div>
         </div>
       </div>
